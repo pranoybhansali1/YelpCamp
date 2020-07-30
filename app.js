7b2860e6551b86f428a1b4da74bed24d6da6a28c
Pranoy Bhansali <pranoybhansali1@gmail.com>
2020-07-30 21:39:59 +0530

    added database

--- app.js
+++ app.js
@@ -1,13 +1,77 @@
-var express = require('express');
-
-var app = express();
-
-var bodyParser = require('body-parser');
+var express = require('express'),
+	app = express(),
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose');
 
 app.use(bodyParser.urlencoded({extended: true}));
-
+mongoose.connect("mongodb://localhost:27017/YelpCamp_Database", {useNewUrlParser: true});
 app.set("view engine", "ejs");
 
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String,
+	description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+app.get("/", function(req, res){
+	res.render("landing");
+});
+
+//INDEX
+app.get("/campgrounds", function(req, res){
+	Campground.find({}, function(err, allCampgrounds){
+		if(err){
+			console.log(err);
+		} else {
+			res.render("index", {campgrounds: allCampgrounds});
+		}
+	});
+});
+
+//NEW
+app.get("/campgrounds/new", function(req, res){
+	res.render("new");
+});
+
+//CREATE
+app.post("/campgrounds", function(req, res){
+	var name = req.body.name;
+	var image = req.body.image;
+	var description = req.body.description;
+	var newCampground = {name: name, image: image, description: description};
+	
+	Campground.create(newCampground, function(err, newlycreated){
+		if(err){
+			console.log(err);
+		} else {
+			res.redirect("/campgrounds");
+		}
+	});
+});
+
+
+//SHOW
+app.get("/campgrounds/:id", function(req, res){
+	Campground.findById(req.params.id, function(err, foundCampground){
+		if(err){
+			console.log(err);
+		} else{
+			res.render("show", {campground: foundCampground});
+		}
+	});
+});
+
+app.listen(3000, function(req, res){
+	console.log("Server has started...");
+});
+
+
+
+
+
+/*
 campgrounds =
 	[
 		{name: "Himachal", image: "https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" },
@@ -17,27 +81,4 @@ campgrounds =
 		{name: "Assam", image: "https://images.unsplash.com/photo-1542067519-6cd1e217df2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" },
 		{name: "Abu", image: "https://images.unsplash.com/photo-1528433556524-74e7e3bfa599?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" }
 	]
-
-app.get("/", function(req, res){
-	res.render("landing");
-});
-
-app.get("/campgrounds", function(req, res){
-	res.render("campgrounds", {campgrounds: campgrounds});
-});
-
-app.get("/campgrounds/new", function(req, res){
-	res.render("new");
-});
-
-app.post("/campgrounds", function(req, res){
-	var name = req.body.name;
-	var image = req.body.image;
-	var newCampground = {name: name, image: image};
-	campgrounds.push(newCampground);
-	res.redirect("/campgrounds");
-});
-
-app.listen(3000, function(req, res){
-	console.log("Server has started...");
-});
+*/
