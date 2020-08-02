f0f5f0849e8a8ce3283dce078bd74e925136a894
Pranoy Bhansali <pranoybhansali1@gmail.com>
2020-08-02 12:40:24 +0530

    updated

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-var express = require('express'),
-	app = express(),
-	bodyParser = require('body-parser'),
-	mongoose = require('mongoose');
+var express 	= require('express'),
+	app 		= express(),
+	bodyParser 	= require('body-parser'),
+	mongoose 	= require('mongoose'),
+	Campground 	= require('./models/campground'),
+	//Comment		= require('./models/comment'),
+	seedDB		= require('./seeds')
+
+
+mongoose.connect("mongodb://localhost:27017/YelpCamp_Database_v3", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to DB!"))
+  .catch((error) => console.log(error.message));
 
 app.use(bodyParser.urlencoded({extended: true}));
-mongoose.connect("mongodb://localhost:27017/YelpCamp_Database", {useNewUrlParser: true});
+
 app.set("view engine", "ejs");
 
-var campgroundSchema = new mongoose.Schema({
-	name: String,
-	image: String,
-	description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
+seedDB();
 
 app.get("/", function(req, res){
 	res.render("landing");
@@ -54,7 +59,7 @@ app.post("/campgrounds", function(req, res){
 
 //SHOW
 app.get("/campgrounds/:id", function(req, res){
-	Campground.findById(req.params.id, function(err, foundCampground){
+	Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
 		if(err){
 			console.log(err);
 		} else{
@@ -62,6 +67,7 @@ app.get("/campgrounds/:id", function(req, res){
 		}
 	});
 });
+
 
 app.listen(3000, function(req, res){
 	console.log("Server has started...");
@@ -81,4 +87,15 @@ campgrounds =
 		{name: "Assam", image: "https://images.unsplash.com/photo-1542067519-6cd1e217df2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" },
 		{name: "Abu", image: "https://images.unsplash.com/photo-1528433556524-74e7e3bfa599?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" }
 	]
+*/
+
+/*
+NAMES:-
+
+your visiting library- current time, location, map, pics, people with, ratings, would you like to show this in public
+hotels and restaurants
+sweets corner
+mbm sugesstions corner, semesters, exams, content
+quora, discord sort of
+resources:- students posting resources helpful for juniors, teachers posting resources, most liked post, most followed post
 */
