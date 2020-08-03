ae4d8faf58169cf4fe4bd0aa4bba4b04efd785bc
Pranoy Bhansali <pranoybhansali1@gmail.com>
2020-08-03 09:07:57 +0530

    styling comments done

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
+app.use(express.static(__dirname + "/public"));
+
 seedDB();
 
 app.get("/", function(req, res){
