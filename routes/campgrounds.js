44c7e7c8991aadbec1ea253d49f41da9978c2923
Pranoy Bhansali <pranoybhansali1@gmail.com>
2020-08-05 15:47:54 +0530

    refactored campgrounds and  comments

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -13,26 +13,30 @@ router.get("/", function(req, res){
 	});
 });
 
+//NEW
+router.get("/new", isLoggedIn, function(req, res){
+	res.render("campgrounds/new");
+});
 
 //CREATE
-router.post("/", function(req, res){
+router.post("/", isLoggedIn, function(req, res){
 	var name = req.body.name;
 	var image = req.body.image;
 	var description = req.body.description;
-	var newCampground = {name: name, image: image, description: description};
+	var author = {
+		id: req.user._id,
+		username: req.user.username
+	};
+	var newCampground = {name: name, image: image, description: description, author: author};
 	
 	Campground.create(newCampground, function(err, newlycreated){
 		if(err){
 			console.log(err);
 		} else {
+			console.log("newly = " + newlycreated);
 			res.redirect("/campgrounds");
 		}
 	});
-});
-
-//NEW
-router.get("/new", function(req, res){
-	res.render("campgrounds/new");
 });
 
 //SHOW
@@ -45,5 +49,13 @@ router.get("/:id", function(req, res){
 		}
 	});
 });
+
+//middleware
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+}
 
 module.exports = router;
